08918d18799a9ca2acc101ac7c79d26ca8c79b92
JairToB <jtorresba@unsa.edu.pe>
2026-07-25 22:53:58 -0500

    feat(storage): implement fixed-page disk manager

--- include/storage/disk_manager.h
+++ include/storage/disk_manager.h
@@ -1,28 +1,38 @@
 #ifndef MINI_DBMS_DISK_MANAGER_H
 #define MINI_DBMS_DISK_MANAGER_H
 
-#include "../common/types.h"
 #include "../common/status.h"
-#include <string>
+#include "../common/types.h"
+
+#include <cstddef>
 #include <fstream>
+#include <mutex>
+#include <string>
 
 namespace minidbms {
-  class DiskManager {
-    public:
-      explicit DiskManager(const std::string& db_file);
-      ~DiskManager();
 
-      Status WritePage(PageId page_id, const char* page_data);
-      Status ReadPage(PageId page_id, char* page_data);
-      PageId AllocatePage();
-      void DeallocatePage(PageId page_id);
+class DiskManager {
+public:
+    explicit DiskManager(const std::string& db_file);
+    ~DiskManager();
 
-    private:
-      std::fstream db_io_;
-      std::string db_filename_;
-      PageId num_pages_{0};
-  };
+    Status WritePage(PageId page_id, const char* page_data);
+    Status ReadPage(PageId page_id, char* page_data);
 
-}
+    PageId AllocatePage();
+    void DeallocatePage(PageId page_id);
+
+    PageId GetNumPages() const;
+    std::size_t GetFileSize() const;
+
+private:
+    std::fstream db_io_;
+    std::string db_filename_;
+    PageId num_pages_{0};
+
+    mutable std::mutex latch_;
+};
+
+} // namespace minidbms
 
 #endif // MINI_DBMS_DISK_MANAGER_H
